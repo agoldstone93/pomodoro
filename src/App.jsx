7c5562ae03804fbf3c8821755020382ab3aeae0e
Adam Goldstone <adam.goldstone@digital.justice.gov.uk>
2023-05-23 15:08:32 +0100

    use react-timer-hook for implementing timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,29 @@
 import './App.css'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { SetTimes } from './SetTimes'
+import { useTimer } from 'react-timer-hook'
 
 function App() {
   const [statusName, setStatusName] = React.useState('Session')
   const [breakLength, setBreakLength] = React.useState(5)
   const [sessionLength, setSessionLength] = React.useState(25)
-  const [timeLeft, setTimeLeft] = React.useState(25)
-  const [isRunning, setIsRunning] = React.useState(false)
-
-  const handleReset = () => {
-    setBreakLength(5)
-    setSessionLength(25)
-    setIsRunning(false)
-    setTimeLeft(25)
-    clearTimeout()
-  }
   
-  const handleStart = () => {
-    setIsRunning((prevIsRunning) => {
-      if (prevIsRunning == true)
-        return false
-      else
-        return true
-    })
-  }
+  const time = new Date();
+  time.setSeconds(time.getSeconds() + sessionLength); // 10 minutes timer
   
-  useEffect(() => {
-    if(isRunning && timeLeft > 0) {
-      setTimeout(() => {
-        setTimeLeft((prevTimeLeft) => prevTimeLeft - 1)
-      }, 1000)
-    } else {
-      clearTimeout()
-    }
-  }, [timeLeft, isRunning])
+  const {
+    seconds,
+    minutes,
+    isRunning,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ expiryTimestamp: time, autoStart: false, onExpire: () => console.warn('onExpire called') });
+  
+  const handleStartStop = () => (isRunning ? pause() : resume())
+  
+  // make always 2 digits, fill zeros at start if necessary
+  const twoDigitTime = (time) => String(time).padStart(2, '0')
 
   return (
     <div className="App">
@@ -42,14 +31,27 @@ function App() {
         <SetTimes name="Break" setBreakLength={setBreakLength} breakLength={breakLength} isRunning={isRunning} />
         <SetTimes name="Session" setSessionLength={setSessionLength} sessionLength={sessionLength} isRunning={isRunning} />
       </div>
+
       <div className='bottom-row'>
         <p id='timer-label'>{statusName}</p>
-        <p id='time-left'>{timeLeft}</p>
+        <div>
+          <span>{twoDigitTime(minutes)}</span>
+          :
+          <span>{twoDigitTime(seconds)}</span>
+        </div>
+        <p>{isRunning ? 'Running' : 'Not running'}</p>
         <div className='start-stop-container'>
-          <button id='start_stop' onClick={handleStart}>
+
+          <button onClick={handleStartStop}>
             { isRunning ? 'Stop' : 'Start' }
           </button>
-          <button id='reset' onClick={handleReset}>Reset</button>
+          <button onClick={() => {
+            // Restarts to 5 minutes timer
+            const time = new Date();
+            time.setSeconds(time.getSeconds() + 5);
+            
+            restart(time, false)
+          }}>Reset</button>
         </div>
       </div>
     </div>
